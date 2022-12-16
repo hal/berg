450db7be88f819f7ea20d1e2e101bfdfb6746b4e
Simon Priadka <spriadka@redhat.com>
2022-12-16 05:42:46 +0100

    hal-resources is copying files

--- packages/hal-resources/src/index.ts
+++ packages/hal-resources/src/index.ts
@@ -1,3 +1,23 @@
 import maven from "maven";
+import path from "path";
 
-maven.create();
+const fixturesDirectory = path.resolve(
+  __dirname,
+  "..",
+  "..",
+  "hal-testsuite",
+  "cypress",
+  "fixtures"
+);
+
+const jdbcDriversDirectory = path.resolve(fixturesDirectory, "jdbc-drivers");
+
+const mavenInstance = maven.create({
+  cwd: path.resolve(__dirname, ".."),
+});
+(async () => {
+  await mavenInstance.execute(["dependency:copy-dependencies"], {
+    excludeTransitive: "true",
+    outputDirectory: jdbcDriversDirectory,
+  });
+})();
