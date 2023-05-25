9a13c2555ecd4d9c69a70bcd09396f85efce534e
Simon Priadka <spriadka@redhat.com>
2023-05-25 14:18:25 +0200

    introduced more constants

--- packages/testsuite/cypress/e2e/runtime/test-runtime-server-status.cy.ts
+++ packages/testsuite/cypress/e2e/runtime/test-runtime-server-status.cy.ts
@@ -2,16 +2,22 @@ describe("TESTS: Runtime => Server => Status", () => {
   let managementEndpoint: string;
   let serverHost: string;
 
+  const memoryAddress = ["core-service", "platform-mbean", "type", "memory"];
+  const threadingAddress = ["core-service", "platform-mbean", "type", "threading"];
+  const heapMemoryUsage = "heap-memory-usage";
+  const nonHeapMemoryUsage = "non-heap-memory-usage";
+
+  const usedHeapProgressBarSelector = "#server-runtime-status-heap-used div.progress-bar-success";
+  const commitedHeapProgressBarSelector = "#server-runtime-status-heap-committed div.progress-bar-success";
+  const usedNonHeapProgressBarSelector = "#server-runtime-status-non-heap-used div.progress-bar-success";
+  const commitedNonHeapProgressBarSelector = "#server-runtime-status-non-heap-committed div.progress-bar-success";
+  const threadsProgressBarSelector = "#server-runtime-status-threads div.progress-bar";
+
   before(() => {
     cy.startWildflyContainer().then((result) => {
       managementEndpoint = result as string;
-      cy.task("execute:cli", {
-        managementApi: managementEndpoint + "/management",
-        operation: "read-attribute",
-        address: [],
-        name: "name",
-      }).then((result) => {
-        serverHost = (result as { result: string }).result;
+      cy.readAttributeAsString(managementEndpoint, [], "name").then((result) => {
+        serverHost = result;
       });
     });
   });
@@ -29,11 +35,7 @@ describe("TESTS: Runtime => Server => Status", () => {
 
   it("Read used heap", () => {
     cy.get("#server-runtime-status-heap-used").should("be.visible");
-    cy.readAttributeAsObject(
-      managementEndpoint,
-      ["core-service", "platform-mbean", "type", "memory"],
-      "heap-memory-usage"
-    ).then((result) => {
+    cy.readAttributeAsObject(managementEndpoint, memoryAddress, heapMemoryUsage).then((result) => {
       const memoryObject = result as {
         init: number;
         used: number;
@@ -42,12 +44,12 @@ describe("TESTS: Runtime => Server => Status", () => {
       };
       const usedHeapMemory = memoryObject.used / 1024 ** 2;
       const maxMemory = memoryObject.max / 1024 ** 2;
-      cy.get("#server-runtime-status-heap-used div.progress-bar-success")
+      cy.get(usedHeapProgressBarSelector)
         .invoke("attr", "aria-valuenow")
         .then(($attributeValue) => {
           expect(Number($attributeValue)).to.be.closeTo(usedHeapMemory, 1);
         });
-      cy.get("#server-runtime-status-heap-used div.progress-bar-success")
+      cy.get(usedHeapProgressBarSelector)
         .invoke("attr", "aria-valuemax")
         .then(($attributeValue) => {
           expect(Number($attributeValue)).to.be.closeTo(maxMemory, 1);
@@ -57,11 +59,7 @@ describe("TESTS: Runtime => Server => Status", () => {
 
   it("Read commited heap", () => {
     cy.get("#server-runtime-status-heap-committed").should("be.visible");
-    cy.readAttributeAsObject(
-      managementEndpoint,
-      ["core-service", "platform-mbean", "type", "memory"],
-      "heap-memory-usage"
-    ).then((result) => {
+    cy.readAttributeAsObject(managementEndpoint, memoryAddress, heapMemoryUsage).then((result) => {
       const memoryObject = result as {
         init: number;
         used: number;
@@ -70,12 +68,12 @@ describe("TESTS: Runtime => Server => Status", () => {
       };
       const commitedHeapMemory = memoryObject.committed / 1024 ** 2;
       const maxMemory = memoryObject.max / 1024 ** 2;
-      cy.get("#server-runtime-status-heap-committed div.progress-bar-success")
+      cy.get(commitedHeapProgressBarSelector)
         .invoke("attr", "aria-valuenow")
         .then(($attributeValue) => {
           expect(Number($attributeValue)).to.be.closeTo(commitedHeapMemory, 1);
         });
-      cy.get("#server-runtime-status-heap-committed div.progress-bar-success")
+      cy.get(commitedHeapProgressBarSelector)
         .invoke("attr", "aria-valuemax")
         .then(($attributeValue) => {
           expect(Number($attributeValue)).to.be.closeTo(maxMemory, 1);
@@ -85,11 +83,7 @@ describe("TESTS: Runtime => Server => Status", () => {
 
   it("Read used non-heap", () => {
     cy.get("#server-runtime-status-non-heap-used").should("be.visible");
-    cy.readAttributeAsObject(
-      managementEndpoint,
-      ["core-service", "platform-mbean", "type", "memory"],
-      "non-heap-memory-usage"
-    ).then((result) => {
+    cy.readAttributeAsObject(managementEndpoint, memoryAddress, nonHeapMemoryUsage).then((result) => {
       const memoryObject = result as {
         init: number;
         used: number;
@@ -98,12 +92,12 @@ describe("TESTS: Runtime => Server => Status", () => {
       };
       const usedNonHeapMemory = memoryObject.used / 1024 ** 2;
       const maxMemory = memoryObject.max / 1024 ** 2;
-      cy.get("#server-runtime-status-non-heap-used div.progress-bar-success")
+      cy.get(usedNonHeapProgressBarSelector)
         .invoke("attr", "aria-valuenow")
         .then(($attributeValue) => {
           expect(Number($attributeValue)).to.be.closeTo(usedNonHeapMemory, 1);
         });
-      cy.get("#server-runtime-status-non-heap-used div.progress-bar-success")
+      cy.get(usedNonHeapProgressBarSelector)
         .invoke("attr", "aria-valuemax")
         .then(($attributeValue) => {
           expect(Number($attributeValue)).to.be.closeTo(maxMemory, 1);
@@ -113,11 +107,7 @@ describe("TESTS: Runtime => Server => Status", () => {
 
   it("Read commited non-heap", () => {
     cy.get("#server-runtime-status-non-heap-committed").should("be.visible");
-    cy.readAttributeAsObject(
-      managementEndpoint,
-      ["core-service", "platform-mbean", "type", "memory"],
-      "non-heap-memory-usage"
-    ).then((result) => {
+    cy.readAttributeAsObject(managementEndpoint, memoryAddress, nonHeapMemoryUsage).then((result) => {
       const memoryObject = result as {
         init: number;
         used: number;
@@ -126,12 +116,12 @@ describe("TESTS: Runtime => Server => Status", () => {
       };
       const commitedNonHeapMemory = memoryObject.committed / 1024 ** 2;
       const maxMemory = memoryObject.max / 1024 ** 2;
-      cy.get("#server-runtime-status-non-heap-committed div.progress-bar-success")
+      cy.get(commitedNonHeapProgressBarSelector)
         .invoke("attr", "aria-valuenow")
         .then(($attributeValue) => {
           expect(Number($attributeValue)).to.be.closeTo(commitedNonHeapMemory, 1);
         });
-      cy.get("#server-runtime-status-non-heap-committed div.progress-bar-success")
+      cy.get(commitedNonHeapProgressBarSelector)
         .invoke("attr", "aria-valuemax")
         .then(($attributeValue) => {
           expect(Number($attributeValue)).to.be.closeTo(maxMemory, 1);
@@ -141,22 +131,14 @@ describe("TESTS: Runtime => Server => Status", () => {
 
   it("Read daemon threads", () => {
     cy.get("#server-runtime-status-threads").scrollIntoView().should("be.visible");
-    cy.readAttributeAsNumber(
-      managementEndpoint,
-      ["core-service", "platform-mbean", "type", "threading"],
-      "daemon-thread-count"
-    ).then((deamonThreads) => {
-      cy.readAttributeAsNumber(
-        managementEndpoint,
-        ["core-service", "platform-mbean", "type", "threading"],
-        "thread-count"
-      ).then((totalThreads) => {
-        cy.get("#server-runtime-status-threads div.progress-bar")
+    cy.readAttributeAsNumber(managementEndpoint, threadingAddress, "daemon-thread-count").then((deamonThreads) => {
+      cy.readAttributeAsNumber(managementEndpoint, threadingAddress, "thread-count").then((totalThreads) => {
+        cy.get(threadsProgressBarSelector)
           .invoke("attr", "aria-valuenow")
           .then(($attributeValue) => {
             expect(Number($attributeValue)).to.be.closeTo(deamonThreads, 1);
           });
-        cy.get("#server-runtime-status-threads div.progress-bar")
+        cy.get(threadsProgressBarSelector)
           .invoke("attr", "aria-valuemax")
           .then(($attributeValue) => {
             expect(Number($attributeValue)).to.be.closeTo(totalThreads, 1);
