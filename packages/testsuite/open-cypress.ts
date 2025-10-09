af0e0c122968d6ed4655d1cb8e8ddf1c80a0326a
Krystof Stekovic <kstekovi@redhat.com>
2025-10-09 10:18:03 +0200

    Update dependencies, NodeJS and Cypress

--- packages/testsuite/open-cypress.ts
+++ packages/testsuite/open-cypress.ts
@@ -1,10 +1,14 @@
 import * as cypress from "cypress";
+import path from "path";
 
 import { Berg } from "@berg/berg";
 
 (async () => {
   const berg = await Berg.getInstance();
+  const projectRoot = path.resolve(__dirname, "..", "..", "..");
   await cypress.open({
+    project: projectRoot,
+    configFile: path.join(projectRoot, "cypress.config.ts"),
     env: {
       NETWORK_NAME: berg.getNetwork().getName(),
       HAL_CONTAINER_PORT: berg.getHalContainer().getMappedPort(9090),
